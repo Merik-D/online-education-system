0ca4f28cc9cfd05afcfcd78e2766a626091f0135
Merik-D <myroslav.diakunchak@gmail.com>
2025-11-09 12:33:53 +0200

    BE-F2 Configured PostgreSQL and created initial database

--- OnlineEducation/OnlineEducation.Api/Program.cs
+++ OnlineEducation/OnlineEducation.Api/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineEducation.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
